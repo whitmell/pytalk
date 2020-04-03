510095ba730e8b46dd44af5675f52919a3360f2d
Paul Tarau <ptarau@gmail.com>
2020-04-02 23:11:04 -0500

    minor

--- doctalk/params.py
+++ doctalk/params.py
@@ -46,7 +46,7 @@ class talk_params:
 
     # visualization / verbosity control
 
-    self.show_pics = 1  # 1 : just generate files, 2: interactive
+    self.show_pics = 0  # 1 : just generate files, 2: interactive
     self.show_rels = 0
     self.to_prolog = 0
     
