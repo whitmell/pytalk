b3093af3adf7972d9227369b9f6903b4e87d34cc
Paul Tarau <ptarau@gmail.com>
2020-04-23 17:38:25 -0500

    made web dialog agent multi-user (but session stateless)

--- docbot/app.py
+++ docbot/app.py
@@ -5,7 +5,33 @@ from docbot import *
 from waitress import serve
 
 # bot, covering content of given text file
-bot = Bot('../examples/covid.txt')
+bots = {
+  "bfr"	: None,
+  "heaven" : None,
+  "relativity" : None,
+  "cats"	: None,
+  "heli" :None,
+  "tesla" :None,
+  "const"	: None,
+  "hindenburg"	: None,
+  "covid"	: None,
+  "kafka"	: None,
+  "texas"	: None,
+  "ec2"	: None,
+  "logrank"	: None,
+  "toxi"	: None,
+  "einstein"	: None,
+  "peirce" : None,
+  "wasteland"	: None,
+  "geo"	: None,
+  "red"	: None,
+  "wolfram"	: None
+}
+
+def activate_bot(name) :
+  if not bots[name]:
+     bots[name] = Bot("../examples/"+name+".txt")
+  return bots[name]
 
 # the Flask-based Web app
 app = Flask(__name__,static_url_path='/static')
@@ -24,30 +50,25 @@ def get_bot_response():
     passes user text from client form to bot
     gets back answer and returns it to client
     '''
-    global bot
     userText = request.args.get('msg')
-    if "open " in userText:
-      text=userText.replace('.','')
-      text = userText.replace('?', '')
-      ws=text.split(' ')
-      fname=ws[1]
-      try :
-        bot=Bot("../examples/"+fname+".txt")
-        answers=["This document is about: "+bot.keyphrases+" . "
-                "Starting with a short summary: "+bot.summary
-               ]
-        return " ".join(answers)
-      except :
-        return "Sorry, no such document!"
-    elif userText =='summary?':
-      return bot.summary
-    elif userText =='keywords?':
-      return bot.keyphrases
+    if ':' not in userText:
+      return 'Expected "document_name : query ?" as input'
     else :
-      try :
-        return bot.ask(userText)
-      except:
-        return "Sorry, I have no answer to that."
+        fname, query = userText.split(':')
+        fname=fname.strip()
+        query=query.strip()
+        try :
+          bot=activate_bot(fname)
+        except :
+          return "Sorry, no such document found."
+        if "summary" in query :
+          return bot.summary
+        if "keywords" in query:
+         return bot.keyphrases
+        try:
+          return bot.ask(userText)
+        except:
+          return "Sorry, I have no answer to that."
 
 if __name__ == "__main__":
   '''
